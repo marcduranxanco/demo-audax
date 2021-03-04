9e2cb825b648c7a1715590ce9ed6ac4f777cca69
MarcDuran <marcduranxanco@hotmail.com>
2021-03-04 11:00:53 +0100

    UPDATE: list contracts view

--- resources/views/contracts/list.blade.php
+++ resources/views/contracts/list.blade.php
@@ -2,6 +2,24 @@
 
 @section('content')
 <div class="row">
-    Contracts list
+
+    <table class="table table-bordered">
+        <tr>
+            <th>Id contract</th>
+            <th>Contract type</th>
+            <th>Id Customer</th>
+            <th>Customer Name</th>
+        </tr>
+        @foreach ($contracts_user as $contract)
+        <tr>
+            <td>{{ $contract->id }}</td>
+            <td>{{ $contract->type }}</td>
+            <td>{{ $contract->customer_id }}</td>
+            <td>{{ $contract->first_name }} {{ $contract->last_name }}</td>
+        </tr>
+        @endforeach
+    </table>
+
+
 </div>
 @endsection
