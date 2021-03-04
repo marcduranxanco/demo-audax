8ab2198c27c741fe18a930536a12202e45bd6d14
MarcDuran <marcduranxanco@hotmail.com>
2021-03-04 08:31:47 +0100

    ADD: migrations Customer and Contracts

---
+++ database/migrations/2021_03_03_223214_create_contracts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContractsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['fix', 'plain', 'indexed']);
+            $table->timestamps();
+            $table->foreignId('customer_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contracts');
+    }
+}
