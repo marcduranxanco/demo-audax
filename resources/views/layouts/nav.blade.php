54627774b048d9daca62f067838cb76a56392c9b
MarcDuran <marcduranxanco@hotmail.com>
2021-03-04 09:55:45 +0100

    UPDATE: navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,18 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-    <a class="navbar-brand">Audax Contracts</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <a class="navbar-brand">Audax</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">      <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav mr-auto">
         @if (Route::has('login'))
             @auth
-                <li class="nav-item active">
-                    <a class="nav-link" href="{{ url('/home') }}">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/contracts') }}">Contracts</a>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/logout') }}">Logout</a>
                 </li>
