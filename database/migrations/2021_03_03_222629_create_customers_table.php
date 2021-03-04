7a11c966043f337a34af240aab12df010f66df6d
MarcDuran <marcduranxanco@hotmail.com>
2021-03-04 08:43:39 +0100

    UPDATE: fix migrations

--- database/migrations/2021_03_03_222629_create_customers_table.php
+++ database/migrations/2021_03_03_222629_create_customers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomerTable extends Migration
+class CreateCustomersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCustomerTable extends Migration
      */
     public function up()
     {
-        Schema::create('customer', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
